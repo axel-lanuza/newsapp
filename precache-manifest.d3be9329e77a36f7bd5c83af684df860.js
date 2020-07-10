56a62ce807746a9b595bdb6d7e5939423646fc61
Axel Lanuza <axell@zoomcatalog.com>
2020-07-10 14:10:24 -0500

    Updates

---
+++ precache-manifest.d3be9329e77a36f7bd5c83af684df860.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "833fc27553901105439d8f731cb3cf45",
+    "url": "/newsapp/index.html"
+  },
+  {
+    "revision": "85c70c19b068034d2fa5",
+    "url": "/newsapp/static/js/2.8dcc5df7.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/newsapp/static/js/2.8dcc5df7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75e9e9e140fc984d3c95",
+    "url": "/newsapp/static/js/main.42fc59f4.chunk.js"
+  },
+  {
+    "revision": "8a8f787854bf7954fbfc",
+    "url": "/newsapp/static/js/runtime-main.0aa4d005.js"
+  }
+]);
